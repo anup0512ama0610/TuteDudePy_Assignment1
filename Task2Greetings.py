135dd64500bb92e51e592b5be86db7f3b8062817
anup0512ama0610 <anupamasripathy@gmail.com>
2025-05-03 03:37:33 +0530

    Add files via upload

---
+++ Task2Greetings.py
@@ -0,0 +1,5 @@
+fname=input("Enter Your First Name: ")
+lname=input("Enter Your Last Name: ")
+Strconcat=fname+" "+lname
+print("Hello,",Strconcat+"! Welcome to the python program.")
+#print("Hello,",(fname+" "+lname)+"! Welcome to the python program.")
